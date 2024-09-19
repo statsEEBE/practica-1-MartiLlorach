7c220da02be1478d96900b35a8e1ee8534b9dc28
DESKTOP-D8EP478\Marti <mllorach6@gmail.com>
2024-09-19 13:20:56 +0200

    ejercicio 1

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,31 @@
 #Codigo para problema 1
 
-plot(1:10, col="green")
+coches <- mtcars
+qsec_intervalos <- cut(coches$qsec, 4)
+ni <- table(qsec_intervalos)
+fi <- ni/sum(ni)
+Ni <- cumsum(ni)
+Fi <- Ni/sum(ni)
+
+cbind(ni, fi, Ni, Fi)
+
+#APARTADO 1
+promedio_qsec <- mean(coches$qsec) #centro de gravedad
+
+#APARTADO 2
+mediana_drat <- median(coches$drat) #mitad de masa
+
+#APARTADO 3
+primer_quartil <- quantile(coches$drat, 0.25)
+
+#APARTADO 4
+primer_18x100 <- quantile(coches$mpg, 0.18)
+
+#APARTADO 5
+rango_interquartil <- IQR(coches$cyl)
+
+#APARTADO 6
+desviacion_tipica_cyl <- sd(coches$cyl)
+
+#APARTADO 7
+varianza_corregida_qsec <- var(coches$qsec)
