182942f45a9b5bbcdad56006514601874fd90ce0
DESKTOP-D8EP478\Marti <mllorach6@gmail.com>
2024-09-19 13:41:37 +0200

    anotacion editada

--- Problema3.R
+++ Problema3.R
@@ -4,7 +4,7 @@ rm(list=ls())
 X <- (1:30)/30
 X_sum <- sum(X)
 
-Y <- X[-c(21, 2, 26)] #la 'c' para hacer vectores!
+Y <- X[-c(21, 2, 26)] #la 'c' para hacer vectores, concatena numeros
 
 calculation <- sum(exp(X)) - sum(exp(Y))
 
